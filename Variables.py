66b3459982dcfb27bf63734c1ce3e0de4f583802
Nikolayka333 <nikolayka333@gmail.com>
2024-11-07 12:23:34 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,13 @@
+# подготовка к решению
+kolichestvo_vipolnennih_DZ = 12
+print(kolichestvo_vipolnennih_DZ)
+kolichestvo_zatrachennih_chasov = 1.5
+print(kolichestvo_zatrachennih_chasov)
+nazvanie_kursa = 'Python'
+print(nazvanie_kursa)
+vremya_na_odno_zadanie = kolichestvo_zatrachennih_chasov / kolichestvo_vipolnennih_DZ
+print(vremya_na_odno_zadanie)
+# конечный результат
+print(nazvanie_kursa + ',','всего задач:' + str(kolichestvo_vipolnennih_DZ) + ',','затрачено часов:' + str(kolichestvo_zatrachennih_chasov) + ',','среднее время выполнения:' + str(vremya_na_odno_zadanie) + ' часа.')
+
+
